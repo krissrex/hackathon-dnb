1937621c727b0adad20e2bed31ca875741e6d8d0
Kristian Rekstad <krirek@msn.com>
2017-09-17 00:44:48 +0200

    Add dummy atm.
    Does not use DNB API

--- src/atm.js
+++ src/atm.js
@@ -1,10 +1,51 @@
-const nearestATM = (ctx) => {
-  console.log(parseInt(ctx.query.longitude, 10));
-  console.log(parseInt(ctx.query.latitude, 10));
-  ctx.body = {
-    longitude: 59.910945,
-    latitude: 10.751762,
-  };
+/** @return {Promise<string>} */
+const convertPositionToZip = (lat, long) => {
+  return new Promise((resolve, reject) => {
+    return resolve('0258');
+  });
 };
 
-export { nearestATM };
+/** @typedef Position
+ * @prop {number} lat
+ * @prop {number} long
+ */
+
+/**
+ * @param {string} zipCode string of numbers 
+ * @return {Promise<Position[]>}
+ */
+const findAtmsFromZip = (zipCode) => {
+  return new Promise((resolve, reject) => {
+
+    return resolve([{ lat: 10, long: 10 }]);
+  });
+};
+
+/** @return {Promise<Position>} */
+const findClosestAtm = (lat, long, atms) => {
+  return new Promise((resolve, reject) => {
+    return resolve(atms[0]);
+  });
+};
+
+const nearestATM = (ctx) => {
+  const long = parseInt(ctx.query.longitude, 10);
+  const lat = parseInt(ctx.query.latitude, 10);
+  console.log('Lat/long:', lat, long);
+
+  convertPositionToZip(lat, long)
+    .then(findAtmsFromZip)
+    .then(atms => findClosestAtm(lat, long, atms))
+    .then((atm) => {
+      ctx.body = {
+        longitude: atm.lat,
+        latitude: atm.long,
+      };
+    })
+    .catch((err) => {
+      ctx.throw(500, 'Failed to find atm', { lat, long, err });      
+      console.error(err);
+    });
+};
+
+export default { nearestATM };
