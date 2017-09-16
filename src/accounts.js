01eef30f0886606e43560d5a46f25d19b58d880e
Kristian Rekstad <krirek@msn.com>
2017-09-17 01:58:39 +0200

    Add sample data

--- src/accounts.js
+++ src/accounts.js
@@ -32,10 +32,86 @@ const balance = async (ctx) => {
   ctx.body = current(customer.accounts).availableBalance;
 };
 
+/**
+ * Sample output: 
+ * 
+ * ```{
+  "customerID": "12039296822",
+  "accounts": [
+    {
+      "accountNumber": 12084059280,
+      "accountType": "Current",
+      "availableBalance": "80810.80"
+    },
+    {
+      "accountNumber": 12084191063,
+      "accountType": "Current",
+      "availableBalance": "145549.00"
+    },
+    {
+      "accountNumber": 12084348562,
+      "accountType": "Current",
+      "availableBalance": "131764.00"
+    },
+    {
+      "accountNumber": 12084749713,
+      "accountType": "Savings",
+      "availableBalance": "106865.00"
+    },
+    {
+      "accountNumber": 12084941549,
+      "accountType": "Current",
+      "availableBalance": "123199.00"
+    }
+  ]
+}```
+ */
 const accounts = async (ctx) => {
   ctx.body = await fetch(getCustomerAccounts(demoCustomer));
 };
 
+/**
+ * Sample data:
+ * 
+ * ```
+
+    {
+      "transactionID": 7188093189,
+      "transactionType": 2,
+      "timeStamp": "2017-01-25, 08:22",
+      "amount": "-2474.00",
+      "message/KID": "4660000000000000",
+      "transactionAccountNumber": 14083089230,
+      "transactionAccountName": "Kontxt AS"
+    },
+    {
+      "transactionID": 7271926491,
+      "transactionType": 1,
+      "timeStamp": "2017-02-27, 13:06",
+      "amount": "-8069.00",
+      "message/KID": "Orange",
+      "transactionAccountNumber": 12084058944,
+      "transactionAccountName": "Solveig Lindqvist"
+    },
+    {
+      "transactionID": 7635724534,
+      "transactionType": 1,
+      "timeStamp": "2017-03-26, 04:10",
+      "amount": "-4975.00",
+      "message/KID": "Yellow",
+      "transactionAccountNumber": 12084386137,
+      "transactionAccountName": "Linda Haaland"
+    },
+    {
+      "transactionID": 6861313898,
+      "transactionType": 5,
+      "timeStamp": "2017-02-07, 21:49",
+      "amount": "-604.46",
+      "message/KID": "Varekjøp Joeandthejuice Nedre Slottsgate Oslo Dato 07.02 kl. 21.49"
+    },
+```
+ *  
+ */
 const transactions = async (ctx) => {
   const customer = await fetch(getCustomerAccounts(demoCustomer));
   const account = current(customer.accounts);
