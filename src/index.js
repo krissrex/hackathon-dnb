0bb34daf77165af0370ec22d0338ceda968194b1
YumYumNoodle <jonathan.s.linnestad@gmail.com>
2017-09-16 13:52:16 +0200

    Add request and stuff

--- src/index.js
+++ src/index.js
@@ -2,11 +2,29 @@ import Koa from 'koa';
 import Router from 'koa-router';
 import Cors from 'kcors';
 import Logger from 'koa-logger';
-
-import Transfer from './transfer.js';
+import request from 'request';
+import rp from 'request-promise';
+import Transfer from './transfer';
 
 
 const app = new Koa();
 app.use(Logger());
 
-app.use(route, ())
+const options = {
+  uri: 'https://dnbapistore.com/hackathon/customers/1.0/customer/12039296822',
+  headers: {
+    'Authorization': 'Bearer 5cdc9b46-b248-3cf3-ba15-aba91ce75f46',
+    'Accept': 'application/json',
+  },
+  json: true, // Automatically parses the JSON string in the response
+};
+
+const yo = rp(options)
+  .then(function (data) {
+      console.log(data);
+  })
+  .catch(function (err) {
+      console.log(err);
+  });
+
+app.listen(3000);
