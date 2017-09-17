aa4a3c4b0639b1594c2c0b3a0872ef07a22bf959
jonasdammen <jodammen93@gmail.com>
2017-09-17 02:20:06 +0200

    started on greenChoice algorithm

--- src/globals.js
+++ src/globals.js
@@ -1,6 +1,65 @@
 class Globals {
   constructor() {
     this.faqList = [];
+    this.personalTransactions = [
+      {
+        'transactionID':6817379073,
+        'transactionType': 4,
+        'timeStamp': '2017-01-01, 07:29',
+        'amount': '-828.98',
+        'message/KID':'* 1812 18.06 Nok 828,976985466041 Vipps by DNB Kurs:1.0000'
+      },
+      {
+        'transactionID':6824063631,
+        'transactionType':5,
+        'timeStamp':'2017-01-06, 14:56',
+        'amount':'-888.52',
+        'message/KID':'Varekjøp Ruter Jernbanetorget Oslo Dato 06.01 kl. 14.56'
+      },
+      {
+        'transactionID':6980843253,
+        'transactionType':4,
+        'timeStamp':'2017-01-02, 22:54','amount':'-1533.83','message/KID':'VISA 3486042 Nok 1533,83 Lekmer.no'
+      },
+      {
+        'transactionID':6803298832,'transactionType':5,'timeStamp':'2017-01-02, 14:47','amount':'-1950.09','message/KID':'Varekjøp NSB Jernbanetorget Oslo Dato 02.01 kl. 14.47'
+      },
+    ];
+    this.othersTransactions = [
+      {
+        'transactionID':6817379073,
+        'transactionType': 4,
+        'timeStamp': '2017-01-01, 07:29',
+        'amount': '-828.98',
+        'message/KID':'* 1812 18.06 Nok 828,976985466041 Vipps by DNB Kurs:1.0000'
+      },
+      {
+        'transactionID':6824063631,
+        'transactionType':5,
+        'timeStamp':'2017-01-06, 14:56',
+        'amount':'-888.52',
+        'message/KID':'Varekjøp Ruter Jernbanetorget Oslo Dato 06.01 kl. 14.56'
+      },
+      {
+        'transactionID':6980843253,
+        'transactionType':4,
+        'timeStamp':'2017-01-02, 22:54','amount':'-1533.83','message/KID':'VISA 3486042 Nok 1533,83 Lekmer.no'
+      },
+      {
+        'transactionID':6803298832,'transactionType':5,'timeStamp':'2017-01-02, 14:47','amount':'-1950.09','message/KID':'Varekjøp NSB Jernbanetorget Oslo Dato 02.01 kl. 14.47'
+      },
+    ];
+    this.branchMaps = {
+      foodStore: ['Rema1000', 'Coop', 'Kiwi', 'Joker'],
+      fastFood: ['McDonalds', 'BurgerKing'],
+      transportation: ['Ruter', 'NSB'],
+      electronics: ['Elkjøp', 'Lefdal', 'Power'],
+      restaurants: ['TGIFriday', 'BambusSushi', 'KulturHuset', 'EspressoHouse', 'joeandthejuice'],
+      div: ['Kicks', 'Ikea', 'Vipps'],
+    };
+    this.branchEmissions = {
+      
+    }
   }
 }
 
---
+++ src/reduceFootprint.js
@@ -0,0 +1,98 @@
+import Globals from './globals';
+
+const persTrans = Globals.personalTransactions;
+const othersTrans = Globals.othersTransactions;
+const brances = Globals.branchMaps;
+
+
+const createMapping = () => {
+  for (let i = 0; i < persTrans.length; i += 1) {
+    const descripton = persTrans[i].descripton;
+    Object.keys(branches).forEach(function(branch) {
+      for (let j = 0; j < branch.length; j += 1) {
+        if (descripton.includes(branch[i])){
+          persTrans[i].branch = branch;
+          break;
+        }
+      }
+
+};
+
+const getSugestionForGreenFootprint = () => {
+  for (let i = 0; i < persTrans.length; i += 1) {
+    const a = persTrans[i].descripton;
+
+  }
+  return (
+    {
+      totaltForbruk: 800000,
+      totaltSpart: 200000,
+      savings: [
+        {
+          from: 'coop',
+          to: 'rema1000',
+          totalSaved: 100000,
+        },
+        {
+          from: 'Lefdal',
+          to: 'Power',
+          totalSaved: 60000,
+        },
+        {
+          from: 'BurgerKing',
+          to: 'McDonalds',
+          totalSaved: 40000,
+        },
+      ],
+      data: [
+        {
+          companyFrom: {
+            name: 'coop',
+            amount: 700000,
+            users: 500,
+            frequency: 0.7,
+          },
+          companyTo: {
+            name: 'rema1000',
+            amount: 100000,
+            users: 500,
+            frequency: 0.7,
+          },
+          diff: 1000,
+        },
+        {
+          companyFrom: {
+            name: 'coop',
+            amount: 700000,
+            users: 500,
+            frequency: 0.7,
+          },
+          companyTo: {
+            name: 'rema1000',
+            amount: 100000,
+            users: 500,
+            frequency: 0.7,
+          },
+          diff: 1000,
+        },
+        {
+          companyFrom: {
+            name: 'coop',
+            amount: 700000,
+            users: 500,
+            frequency: 0.7,
+          },
+          companyTo: {
+            name: 'rema1000',
+            amount: 100000,
+            users: 500,
+            frequency: 0.7,
+          },
+          diff: 1000,
+        },
+      ],
+    }
+  );
+};
+
+export { getSugestionForGreenFootprint };
